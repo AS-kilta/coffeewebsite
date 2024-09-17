7cc90473159c624d36eb49052d39cbd7ad99c270
Ileska <ileska@tutanota.com>
2024-09-17 17:07:35 +0300

    Add getPic path difference

--- getPic.py
+++ getPic.py
@@ -5,7 +5,7 @@ from decouple import config
 
 TAPO_USERNAME = config("TAPO_USERNAME")
 TAPO_PASSWORD = config("TAPO_PASSWORD")
-IMG_PATH = config("TAPO_PASSWORD")
+IMG_PATH = config("IMG_PATH")
 
 def getImage() -> bool:
 	HOST = "192.168.1.45" # The server's hostname or IP address
@@ -14,6 +14,7 @@ def getImage() -> bool:
 	url = f"rtsp://{TAPO_USERNAME}:{TAPO_PASSWORD}@{HOST}:{PORT}/stream1"
 	savePath = IMG_PATH
 
+	print(savePath)
 	try:
 		stream = cv2.VideoCapture(url)
 	except Exception as g:
